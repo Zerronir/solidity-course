07b40f1270356d9f87018c5b17c9001f5e45b9fc
Raúl García-Verdugo Cardador <60638702+Zerronir@users.noreply.github.com>
2022-03-24 11:08:52 +0100

    Update ejemplo3.sol

--- ejemplo3.sol
+++ ejemplo3.sol
@@ -2,6 +2,25 @@
 pragma solidity >0.7.0 <0.9.0;
 
 contract ejemplo3 {
+
+    address contrato = address(this);
+    address payable p_contrato = payable(contrato); // Hacemos que la address que hemos definido antes sea payable
+
+    event transferred(address);
+
+    function etherToContract() public payable {
+        p_contrato.transfer(msg.value);
+        emit transferred(msg.sender);
+    }
+
+    function getBalance() public view returns(uint256) {
+        return(contrato.balance);
+    }
+
+    receive() external payable {
+
+    }
+
     /*
     * Función para realizar una transferencia de ethers entre cuentas
     */
@@ -13,5 +32,4 @@ contract ejemplo3 {
     function checkBalance(address addr) public view returns(uint256) {
         return(addr.balance);
     }
-
 }
